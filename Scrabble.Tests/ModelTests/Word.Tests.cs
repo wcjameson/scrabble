949cea6a18a08162fbb78b66cbce6a117bd86f81
wcjameson <williamjameson90@gmail.com>
2022-02-24 12:01:24 -0800

    WIP characterScore method
    Co-authored-by: Matt Eilar <meilar@gmail.com>

--- Scrabble.Tests/ModelTests/Word.Tests.cs
+++ Scrabble.Tests/ModelTests/Word.Tests.cs
@@ -12,5 +12,12 @@ public void WordConstructor_InstantiatesWord_AWord()
   Word testObject = new Word("test");
   Assert.AreEqual(typeof(Word), testObject.GetType());
 }
+[TestMethod]
+public void TallyScore_CalculateScore_AScore()
+{
+  Word testWord = new Word("bed");
+  Assert.AreEqual(6, testWord.Score);
+  
+}
   }
 }
--- Scrabble/Models/Word.cs
+++ Scrabble/Models/Word.cs
@@ -1,3 +1,7 @@
+using System.Collections.Generic;
+using System.Text.RegularExpressions;
+using System;
+
 namespace Scrabble.Models
 {
   public class Word
@@ -15,7 +19,31 @@ namespace Scrabble.Models
     private int TallyScore(string term) 
     {
       int score = 0;
+      char[] characterArray = term.ToCharArray();
+
+      foreach(string character in characterArray)
+      {
+        int charScore = CharacterScore(character);
+        score = score + charScore;
+      }
       return score;
     }
+
+    public int CharacterScore(string character)
+    {
+      Regex test1 = new Regex(@"/a|e|i|o|u|l|n|r|s|t/gmi");
+      Match match1 = test1.Match(character);
+      if (match1.Success)
+      {
+        return 1;
+      } 
+      else
+      {
+        return 0;
+      } 
+
+    }
+
+
   }
 }
