dbc4f14fdc6b88f0be0ca0e9d332518d42207b6f
wcjameson <williamjameson90@gmail.com>
2022-02-24 14:22:10 -0800

    add user interface
    Co-authored-by: Matt Eilar <meilar@gmail.com>

--- Scrabble/Program.cs
+++ Scrabble/Program.cs
@@ -1,5 +1,17 @@
-// using System;
+using System;
+using Scrabble.Models;
 
-// static void Main() {
-  
-// }
+namespace Scrabble
+{
+  public class Program
+  {
+    static void Main()
+    {
+      Console.WriteLine("Enter your Scrabble word!");
+      string scrabbleWord = Console.ReadLine();
+      Word userWord = new Word(scrabbleWord);
+      Console.WriteLine("Your score is " + userWord.Score);
+
+    }
+  }
+}
